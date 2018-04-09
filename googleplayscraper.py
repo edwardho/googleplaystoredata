0b0bf7d6d6d5d5e4c4ed6b8210b54cbf919ac149
Edward Ho <ekinbenho@gmail.com>
2018-04-09 17:57:59 -0400

    Update googleplayscraper.py

--- googleplayscraper.py
+++ googleplayscraper.py
@@ -7,12 +7,11 @@ f = open('androidpackagenames.csv')
 fp = open('googleplayscraperoutput.csv', 'wb')
 reader = csv.reader(f)
 writer = csv.writer(fp, delimiter=',')
+
 accesstoken = raw_input("Please enter your 42matters access_token: ")
 print "You entered the access token: ", accesstoken
 
 writer.writerows([['Android Package Name', 'Publisher Name', 'App Name', 'Primary Category', 'Store URL']])
-
-
 
 for row in reader:
 
@@ -32,20 +31,22 @@ for row in reader:
                         pgenre = (appdata['category'])
                         #print pgenre
                         murl = (appdata['market_url'])
+                        separator = '&referrer'
+                        cleanmurl = murl.split(separator, 1)[0]
                 else:
                         aid = row[0]
                         pubname = 'NULL'
                         appname = 'NULL'
                         pgenre = 'NULL'
-                        murl = 'NULL'
+                        cleanmurl = 'NULL'
 
         except urllib2.HTTPError as e:
                 aid = row[0]
                 pubname = 'NULL'
                 appname = 'NULL'
                 pgenre = 'NULL'
-                murl = 'NULL'
+                cleanmurl = 'NULL'
 
-        writer.writerows([[aid, pubname.encode('utf-8'), appname.encode('utf-8'), pgenre, murl]])
+        writer.writerows([[aid, pubname.encode('utf-8'), appname.encode('utf-8'), pgenre, cleanmurl]])
 
 print "Script run complete. Please check the output googleplayscraperoutput.csv"
